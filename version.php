c41c9b2a7ce722e0ce77aa71817f862ae6f39987
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2012-04-25 08:49:45 +0200

    [Enh][Trivial] Removed build information from $plugin->release

--- version.php
+++ version.php
@@ -27,10 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012031000;      // If version == 0 then module will not be installed
+$plugin->version   = 2012042100;      // If version == 0 then module will not be installed
 $plugin->requires  = 2010112400;      // Requires this Moodle version
 $plugin->cron      = 0;               // Period for cron to check this module (secs)
 $plugin->component = 'local_davroot'; // To check on upgrade, that module sits in correct place
 $plugin->maturity  = MATURITY_RC;     // This version's maturity level
-// Human-friendly version name
-$plugin->release   = '1.0-rc3 (Build: 2012031000)';
+$plugin->release   = '1.0-rc4-2012042100'; // Human-friendly version name
